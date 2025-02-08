3153358ac7fc52cb1fac3863e5f0ec34e79a7c07
DeNero50187 <73082558+DeNero50187@users.noreply.github.com>
2025-02-08 02:15:57 -0500

    Update config.js

--- hyperion-home-assistant-bridge/config.js
+++ hyperion-home-assistant-bridge/config.js
@@ -1,6 +1,10 @@
 module.exports = {
   lights: [
-    { id: "atmos_left", type: "rgb" },
-    { id: "atmos_right", type: "rgb" }
+    { id: "light.dw_front_right", type: "rgb" },
+    { id: "light.dw_mid_right", type: "rgb" },
+    { id: "light.dw_rear_right", type: "rgb" },
+    { id: "light.dw_rear_left", type: "rgb" },
+    { id: "light.dw_mid_left", type: "rgb" },
+    { id: "light.dw_front_left", type: "rgb" }
   ],
 };
